62c57898f4dacb132d8292e484f402c0531b36b5
furlanrapha <furlanrapha@85e3550e-3c49-11de-be05-5f7a86268029>
2009-05-28 19:24:45 +0000

    Telas de Cliente

--- trunk/src/br/iav/ac/telas/cliente/DialogoCliente.java
+++ trunk/src/br/iav/ac/telas/cliente/DialogoCliente.java
@@ -2,87 +2,193 @@ package br.iav.ac.telas.cliente;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
 import br.iav.ac.negocio.Cliente;
 import br.iav.ac.negocio.ListaObjeto;
 import br.iav.ac.telas.padrao.DialogoPadrao;
 
 public class DialogoCliente extends DialogoPadrao {
 
-	/*----------------------------------------------------------
-	 * ATTRIBUTOS
-	 *----------------------------------------------------------*/
-
-	private static final long serialVersionUID = 1L;
 	private JLabel labelNome;
+	private JLabel labelTelefone;
+	private JTextField textTelefone;
 	private JTextField textNome;
+	private JLabel labelCpf;
+	private JTextField textCpf;
+	private JLabel labelRg;
+	private JTextField textRg;
+	private JLabel labelProfissao;
+	private JTextField textProfissao;
+	private JLabel labelRua;
+	private JTextField textRua;
+	private JLabel labelNumero;
+	private JTextField textNumero;
+	private JLabel labelBairro;
+	private JTextField textBairro;
+	private JLabel labelCep;
+	private JTextField textCep;
+	private JLabel labelComplemento;
+	private JTextField textComplemento;
+	private JLabel labelCidade;
+	private JComboBox comboCidade;
 	private FormHandle formHandle;
 	private Cliente cliente;
-
-	/*----------------------------------------------------------
-	 * FIM DE ATTRIBUTOS
-	 *----------------------------------------------------------*/
-
-	/*----------------------------------------------------------
-	 * CONSTRUTOR
-	 *----------------------------------------------------------*/
 
 	public DialogoCliente(JFrame frame, String titulo, boolean modal, Cliente cliente) {
 		super(frame, titulo, modal);
 		this.cliente = cliente;
+		//25 de espaçamento
 		try {
             {
             	labelNome = new JLabel();
             	getPanelPrincipal().add(labelNome);
             	labelNome.setText("Nome:");
-            	labelNome.setBounds(28, 38, 21, 14);
+            	labelNome.setBounds(12, 37, 80, 20);
 	        }
 	        {
 	        	textNome = new JTextField(this.cliente.getNome().trim());
 	        	getPanelPrincipal().add(textNome);
-	        	textNome.setBounds(51, 35, 246, 21);
+	        	textNome.setBounds(90, 35, 246, 21);
+	        }
+            {
+            	labelTelefone = new JLabel();
+            	getPanelPrincipal().add(labelTelefone);
+            	labelTelefone.setText("Telefone:");
+            	labelTelefone.setBounds(12, 62, 80, 20);
+	        }
+	        {
+	        	textTelefone = new JTextField(this.cliente.getTelefone().trim());
+	        	getPanelPrincipal().add(textTelefone);
+	        	textTelefone.setBounds(90, 60, 246, 21);
+	        }
+            {
+            	labelCpf = new JLabel();
+            	getPanelPrincipal().add(labelCpf);
+            	labelCpf.setText("CPF:");
+            	labelCpf.setBounds(12, 87, 80, 20);
+	        }
+	        {
+	        	textCpf = new JTextField(this.cliente.getCpf().trim());
+	        	getPanelPrincipal().add(textCpf);
+	        	textCpf.setBounds(90, 85, 246, 21);
+	        }
+            {
+            	labelRg = new JLabel();
+            	getPanelPrincipal().add(labelRg);
+            	labelRg.setText("RG:");
+            	labelRg.setBounds(12, 112, 80, 20);
+	        }
+	        {
+	        	textRg = new JTextField(this.cliente.getRg().trim());
+	        	getPanelPrincipal().add(textRg);
+	        	textRg.setBounds(90, 110, 246, 21);
+	        }
+            {
+            	labelProfissao = new JLabel();
+            	getPanelPrincipal().add(labelProfissao);
+            	labelProfissao.setText("Profissão:");
+            	labelProfissao.setBounds(12, 137, 80, 20);
+	        }
+	        {
+	        	textProfissao = new JTextField(this.cliente.getProfissao().trim());
+	        	getPanelPrincipal().add(textProfissao);
+	        	textProfissao.setBounds(90, 135, 246, 21);
+	        }
+            {
+            	labelRua = new JLabel();
+            	getPanelPrincipal().add(labelRua);
+            	labelRua.setText("Rua:");
+            	labelRua.setBounds(12, 162, 80, 20);
+	        }
+	        {
+	        	textRua = new JTextField(this.cliente.getEndereco().getRua().trim());
+	        	getPanelPrincipal().add(textRua);
+	        	textRua.setBounds(90, 160, 246, 21);
+	        }
+            {
+            	labelNumero = new JLabel();
+            	getPanelPrincipal().add(labelNumero);
+            	labelNumero.setText("Número:");
+            	labelNumero.setBounds(12, 187, 80, 20);
+	        }
+	        {
+	        	textNumero = new JTextField(String.valueOf(this.cliente.getEndereco().getNumero()));
+	        	getPanelPrincipal().add(textNumero);
+	        	textNumero.setBounds(90, 185, 246, 21);
+	        }
+            {
+            	labelBairro = new JLabel();
+            	getPanelPrincipal().add(labelBairro);
+            	labelBairro.setText("Bairro:");
+            	labelBairro.setBounds(12, 212, 80, 20);
+	        }
+	        {
+	        	textBairro = new JTextField(this.cliente.getEndereco().getBairro().trim());
+	        	getPanelPrincipal().add(textBairro);
+	        	textBairro.setBounds(90, 210, 246, 21);
+	        }
+            {
+            	labelCep = new JLabel();
+            	getPanelPrincipal().add(labelCep);
+            	labelCep.setText("CEP:");
+            	labelCep.setBounds(12, 237, 80, 20);
+	        }
+	        {
+	        	textCep = new JTextField(this.cliente.getEndereco().getCep().trim());
+	        	getPanelPrincipal().add(textCep);
+	        	textCep.setBounds(90, 235, 246, 21);
+	        }
+            {
+            	labelComplemento = new JLabel();
+            	getPanelPrincipal().add(labelComplemento);
+            	labelComplemento.setText("Complemento:");
+            	labelComplemento.setBounds(12, 262, 80, 20);
+	        }
+	        {
+	        	textComplemento = new JTextField(this.cliente.getEndereco().getComplemento().trim());
+	        	getPanelPrincipal().add(textComplemento);
+	        	textComplemento.setBounds(90, 260, 246, 21);
+	        }
+            {
+            	labelCidade = new JLabel();
+            	getPanelPrincipal().add(labelCidade);
+            	labelCidade.setText("Cidade:");
+            	labelCidade.setBounds(12, 287, 80, 20);
+	        }
+	        {
+	        	comboCidade = new JComboBox();
+	        	getPanelPrincipal().add(comboCidade);
+	        	comboCidade.setSelectedItem(cliente.getEndereco().getCidade().getNome().trim());
+	        	comboCidade.setBounds(90, 285, 246, 21);
 	        }
 	        {
 				if (this.cliente.getCodigo() != 0) {
-					getLabelCodigo().setText(getLabelCodigo().getText() + "  " + this.cliente.getCodigo());
+					getTextCodigo().setText(String.valueOf(this.cliente.getCodigo()));
 				}
 	        }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		inicializarHandlers();
-		this.setSize(400, 200);
+		this.setSize(350, 380);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
-
-	/*----------------------------------------------------------
-	 * FIM DE CONSTRUTOR
-	 *----------------------------------------------------------*/
-
-	/*----------------------------------------------------------
-	 * INTERFACE
-	 *----------------------------------------------------------*/
 
 	private void inicializarHandlers() {
 		this.formHandle = new FormHandle();
 		getBotaoCancelar().addActionListener(formHandle);
 		getBotaoConfirmar().addActionListener(formHandle);
 	}
-	
-	/*----------------------------------------------------------
-	 * FIM DE INTERFACE
-	 *----------------------------------------------------------*/
 
-	/*----------------------------------------------------------
-	 * CLASSE LIMITROFE
-	 *----------------------------------------------------------*/
-
-	class FormHandle implements ActionListener {
-
+	class FormHandle implements ActionListener { 
+		
 		public FormHandle() {
 			super();
 		}
@@ -91,8 +197,8 @@ public class DialogoCliente extends DialogoPadrao {
 			textNome.setText("");
 		}
 		
-		private boolean existeCor(){
-			ListaObjeto listaObjeto = cliente.search("cor","Igual",textNome.getText().trim());
+		private boolean existeCliente(){
+			ListaObjeto listaObjeto = cliente.search("cor", "Igual", textNome.getText().trim());
 			if (listaObjeto.getSize() > 0) {
 				return false;
 			}			
@@ -107,14 +213,14 @@ public class DialogoCliente extends DialogoPadrao {
 					JOptionPane.showMessageDialog(DialogoCliente.this, "O campo nome é obrigatório!");
 				} else {
 					if (cliente.getCodigo() == 0) {
-						if (existeCor()) {
+						if (existeCliente()) {
 							cliente.setNome(textNome.getText().trim());
 							cliente.insert();							
 						} else {
 							JOptionPane.showMessageDialog(DialogoCliente.this, "Esse cliente já se encontra na Base de Dados!");
 						}
 					} else {
-						if (existeCor()) {
+						if (existeCliente()) {
 							cliente.setNome(textNome.getText().trim());
 							cliente.edit();	
 						} else{
@@ -127,9 +233,5 @@ public class DialogoCliente extends DialogoPadrao {
 			}
 		}
 	}
-
-	/*----------------------------------------------------------
-	 * FIM DE CLASSE LIMITROFE
-	 *----------------------------------------------------------*/
 
 }
--- trunk/src/br/iav/ac/telas/cliente/PainelCliente.java
+++ trunk/src/br/iav/ac/telas/cliente/PainelCliente.java
@@ -4,124 +4,100 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 import br.iav.ac.negocio.Cliente;
 import br.iav.ac.negocio.ListaObjeto;
 import br.iav.ac.telas.padrao.PainelPadrao;
 
 public class PainelCliente extends PainelPadrao {
 
-		/*----------------------------------------------------------
-		 * ATTRIBUTOS
-		 *----------------------------------------------------------*/
+	private CadastroHandle cadastroHandle;
+	private static String[] CAMPOS = { "Código", "Nome", "Telefone", "CPF", "RG", "Profissão", "Rua", "Número", "Bairro", "CEP", "Complemento", "Cidade" };
 
-		private CadastroHandle cadastroHandle;
-		private Cliente cliente;
-		private static String[] CAMPOS = { "Código", "Nome" };
+	public PainelCliente() {
+		super(CAMPOS);
+		inicializarHandlers();
+	}
 
-		/*----------------------------------------------------------
-		 * FIM DE ATTRIBUTOS
-		 *----------------------------------------------------------*/
-
-		/*----------------------------------------------------------
-		 * CONSTRUTOR
-		 *----------------------------------------------------------*/
-
-		public PainelCliente() {
-			super(CAMPOS);
-			inicializarHandlers();
-		}
-
-		/*----------------------------------------------------------
-		 * FIM DE CONSTRUTOR
-		 *----------------------------------------------------------*/
-
-		/*----------------------------------------------------------
-		 * INTERFACE
-		 *----------------------------------------------------------*/
-
-		private void inicializarHandlers() {
-			this.cadastroHandle = new CadastroHandle();
-			this.getBotaoNovo().addActionListener(cadastroHandle);
-			this.getBotaoEditar().addActionListener(cadastroHandle);
-			this.getBotaoExcluir().addActionListener(cadastroHandle);
-			this.getBotaoBuscar().addActionListener(cadastroHandle);
-			this.getBotaoAtualizar().addActionListener(cadastroHandle);
-		}
+	private void inicializarHandlers() {
+		this.cadastroHandle = new CadastroHandle();
+		this.getBotaoNovo().addActionListener(cadastroHandle);
+		this.getBotaoEditar().addActionListener(cadastroHandle);
+		this.getBotaoExcluir().addActionListener(cadastroHandle);
+		this.getBotaoBuscar().addActionListener(cadastroHandle);
+		this.getBotaoAtualizar().addActionListener(cadastroHandle);
+	}
 			
-		/*----------------------------------------------------------
-		 * FIM DE INTERFACE
-		 *----------------------------------------------------------*/
+	class CadastroHandle implements ActionListener {
 
-		/*----------------------------------------------------------
-		 * CLASSE LIMITROFE
-		 *----------------------------------------------------------*/
+		private Cliente cliente = new Cliente(); 
+		
+		public CadastroHandle() {
+			super();
+			carregarGrid(cliente.load());
+		}
 
-		class CadastroHandle implements ActionListener {
-
-			public CadastroHandle() {
-				super();
-				carregarGrid(getCliente().load());
+		private Cliente buscarCliente(){
+			String nome = getGridTabela().getValueAt(getGridTabela().getSelectedRow(), 1)+ "";
+			ListaObjeto listaObjeto = cliente.search("Nome", "Igual", nome);
+			if (listaObjeto.getSize() > 0) {
+				return (Cliente) listaObjeto.getObjeto(0);				
+			}	
+			return null;			
+		}
+		
+		private void carregarGrid(ListaObjeto listaObjeto) {
+			Object[][] gridArray = new Object[listaObjeto.getSize()][2];
+			for (int i = 0; i < listaObjeto.getSize(); i++) {
+				Cliente cliente = (Cliente) listaObjeto.getObjeto(i);
+				gridArray[i][0] = cliente.getCodigo();
+				gridArray[i][1] = cliente.getNome();
 			}
+			DefaultTableModel model = new DefaultTableModel(gridArray, CAMPOS);
+			getGridTabela().setModel(model);
+			getGridTabela().setShowVerticalLines(true);
+		}
 
-			private Cliente getCliente() {
-				if (cliente == null) {
-					cliente = new Cliente();
-				}
-				return cliente;
-			}
-
-			private void carregarGrid(ListaObjeto listaObjeto) {
-				Object[][] gridArray = new Object[listaObjeto.getSize()][2];
-				for (int i = 0; i < listaObjeto.getSize(); i++) {
-					Cliente cliente = (Cliente) listaObjeto.getObjeto(i);
-					gridArray[i][0] = cliente.getCodigo();
-					gridArray[i][1] = cliente.getNome();
-				}
-				DefaultTableModel model = new DefaultTableModel(gridArray, CAMPOS);
-				getGridTabela().setModel(model);
-				getGridTabela().setShowVerticalLines(true);
-				TableColumn column = getGridTabela().getColumnModel().getColumn(0);
-				column.setPreferredWidth(100);
-				column = getGridTabela().getColumnModel().getColumn(1);
-				column.setPreferredWidth(500);
-			}
-
-			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == getBotaoNovo()) {
-					this.getCliente().setCodigo(0);
-					this.getCliente().setNome("");
-					new DialogoCliente(null, "Cadastro de Cliente", true, this.getCliente());
-					carregarGrid(getCliente().load());
-				} else if (e.getSource() == getBotaoEditar()) {
-					if (getGridTabela().getSelectedRow() >= 0) {
-						this.getCliente().setCodigo((Integer) getGridTabela().getValueAt(getGridTabela().getSelectedRow(), 0));
-						this.getCliente().setNome(getGridTabela().getValueAt(getGridTabela().getSelectedRow(), 1)+ "");
-						new DialogoCliente(null, "Cadastro de Cliente", true, this.getCliente());
-						carregarGrid(getCliente().load());	
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == getBotaoNovo()) {
+				this.cliente.setCodigo(0);
+				this.cliente.setNome("");
+				new DialogoCliente(null, "Cadastro de Cliente", true, this.cliente);
+				carregarGrid(cliente.load());
+			} else if (e.getSource() == getBotaoEditar()) {
+				if (getGridTabela().getSelectedRow() >= 0) {
+					cliente = buscarCliente();
+					if (cliente != null) {
+						new DialogoCliente(null, "Cadastro de Cliente", true, this.cliente);
+						carregarGrid(cliente.load());	
 					} else {
-						JOptionPane.showMessageDialog(PainelCliente.this, "Para editar é preciso selecionar um cliente na tabela!");
+						JOptionPane.showMessageDialog(PainelCliente.this, "Erro ao buscar este cliente na base de dados!");
+						cliente = new Cliente();
 					}
-				} else if (e.getSource() == getBotaoExcluir()) {
-					if (getGridTabela().getSelectedRow() > 0) {
-						if (JOptionPane.showConfirmDialog(null,"Deseja mesmo excluir o cliente " + getGridTabela().getValueAt(getGridTabela().getSelectedRow(), 1) + " ?", "Exclusão", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-							getCliente().setCodigo((Integer) getGridTabela().getValueAt(getGridTabela().getSelectedRow(), 0));
-							getCliente().delete();
-							carregarGrid(getCliente().load());
+				} else {
+					JOptionPane.showMessageDialog(PainelCliente.this, "Para editar é preciso selecionar um cliente na tabela!");
+				}
+			} else if (e.getSource() == getBotaoExcluir()) {
+				if (getGridTabela().getSelectedRow() >= 0) {
+					cliente = buscarCliente();
+					if (cliente != null) {
+						int resp = JOptionPane.showConfirmDialog(null, "Deseja mesmo excluir o cliente " + cliente.getNome()+ " ?", "Exclusão", JOptionPane.YES_NO_OPTION);
+						if (resp == 0) {
+							cliente.delete();
+							carregarGrid(cliente.load());
 						}
 					} else {
-						JOptionPane.showMessageDialog(PainelCliente.this, "Para remover é preciso selecionar uma cor na tabela!");
+						JOptionPane.showMessageDialog(PainelCliente.this, "Erro ao buscar este cliente na base de dados!");
+						cliente = new Cliente();
 					}
-				} else if (e.getSource() == getBotaoAtualizar()) {
-					carregarGrid(getCliente().load());
-				} else if (e.getSource() == getBotaoBuscar()) {		
-					carregarGrid(getCliente().search((String) getComboAtributoBuscar().getSelectedItem(), (String) getComboTipoBuscar().getSelectedItem(), getTextBuscar().getText()));
+				} else {
+					JOptionPane.showMessageDialog(PainelCliente.this, "Para remover é preciso selecionar uma cor na tabela!");
 				}
+			} else if (e.getSource() == getBotaoAtualizar()) {
+				carregarGrid(cliente.load());
+			} else if (e.getSource() == getBotaoBuscar()) {		
+				carregarGrid(cliente.search((String) getComboAtributoBuscar().getSelectedItem(), (String) getComboTipoBuscar().getSelectedItem(), getTextBuscar().getText()));
 			}
 		}
-
-		/*----------------------------------------------------------
-		 * FIM DE CLASSE LIMITROFE
-		 *----------------------------------------------------------*/
-
 	}
+
+}
