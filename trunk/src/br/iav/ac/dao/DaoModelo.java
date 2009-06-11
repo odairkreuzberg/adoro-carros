8b72244aaa82f38bb51a9e3532ebe6ebc970955e
furlanrapha <furlanrapha@85e3550e-3c49-11de-be05-5f7a86268029>
2009-06-11 19:57:16 +0000

    Alteração na DaoModelo.

--- trunk/src/br/iav/ac/dao/DaoModelo.java
+++ trunk/src/br/iav/ac/dao/DaoModelo.java
@@ -14,8 +14,7 @@ public class DaoModelo implements DaoInterface {
 	// Nome da tabela e nome do sufixo do código
 	private final static String tableName = "modelo";
 
-	private final static String SELECT = "select modelo.*, marca.nome as ma_nome"
-			+ " from marca inner join modelo on (modelo.cod_marca = marca.cod_marca) ";
+	private final static String SELECT = "select modelo.*, marca.nome as ma_nome" + " from marca inner join modelo on (modelo.cod_marca = marca.cod_marca) ";
 
 	public Modelo getModelo() {
 		return modelo;
@@ -27,38 +26,26 @@ public class DaoModelo implements DaoInterface {
 
 	public void delete() {
 		if (db.connect()) {
-			db.update("delete from " + tableName + " where cod_" + tableName
-					+ " = " + modelo.getCodigo());
+			db.update("delete from " + tableName + " where cod_" + tableName + " = " + modelo.getCodigo());
 			db.disconnect();
 		}
 	}
 
 	public void edit() {
 		if (db.connect()) {
-			db.update("update " + tableName + " set nome = '"
-					+ modelo.getNome() + "', cod_marca = "
-					+ modelo.getMarca().getCodigo() + " where cod_" + tableName
-					+ " = " + modelo.getCodigo());
+			db.update("update " + tableName + " set " +
+											  "nome = '" + modelo.getNome() + "', " +
+											  "cod_marca = " + modelo.getMarca().getCodigo() + " " +
+											  "where cod_" + tableName	+ " = " + modelo.getCodigo());
 			db.disconnect();
 		}
 	}
 
 	public void insert() {
 		if (db.connect()) {
-			db.update("insert into " + tableName
-					+ " (nome, cod_marca) values ('" + modelo.getNome() + "', "
-					+ modelo.getMarca().getCodigo() + ")");
+			db.update("insert into " + tableName + " (nome, cod_marca) values ('" + modelo.getNome() + "', " + modelo.getMarca().getCodigo() + ")");
 			db.disconnect();
 		}
-	}
-
-	public Modelo searchWithCodigo(int codigo) {
-		ListaObjeto listaObjeto = this.search("Código", "Igual", String
-				.valueOf(codigo));
-		if (listaObjeto.getSize() == 1) {
-			return (Modelo) listaObjeto.getObjeto(0);
-		}
-		return null;
 	}
 
 	public ListaObjeto load(String sql) {
@@ -84,27 +71,31 @@ public class DaoModelo implements DaoInterface {
 		return this.load(SELECT);
 	}
 
+	public Modelo searchWithCodigo(int codigo) {
+		ListaObjeto listaObjeto = this.search("Código", "Igual", String.valueOf(codigo));
+		if (listaObjeto.getSize() == 1) {
+			return (Modelo) listaObjeto.getObjeto(0);
+		}
+		return null;
+	}
+	
 	public ListaObjeto search(String campo, String operador, String valor) {
-
 		String sql = SELECT;
-
 		String campoSQL = null;
 		String operadorSQL = null;
 		String valorSQL = "'" + valor + "'";
-
 		if (campo.equals("Código")) {
-			campoSQL = "where cod_modelo ";
+			campoSQL = " where cod_modelo ";
 			valorSQL = valor;
 			if (operador.equals("Contem")) {
 				operador = "Igual";
 				valorSQL = "-1";
 			}
 		} else if (campo.equals("Marca")) {
-			campoSQL = "where marca.nome ";
+			campoSQL = " where marca.nome ";
 		} else if (campo.equals("Modelo")) {
-			campoSQL = "where modelo.nome ";
+			campoSQL = " where modelo.nome ";
 		}
-
 		if (operador.equals("Igual")) {
 			operadorSQL = "=";
 		} else if (operador.equals("Diferente")) {
@@ -117,15 +108,12 @@ public class DaoModelo implements DaoInterface {
 			operadorSQL = "like";
 			valorSQL = " '%" + valor + "%'";
 		}
-
 		sql += campoSQL + " " + operadorSQL + valorSQL;
 		return this.load(sql);
 	}
 
 	public boolean temMarca(int codigo) {
-
-		return this.search("cod_marca", "Igual", String.valueOf(codigo))
-				.getSize() > 0;
+		return this.search("cod_marca", "Igual", String.valueOf(codigo)).getSize() > 0;
 	}
 
 }
